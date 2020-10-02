5bd1b00cb1eae36fa200b46588cf573c60908378
kenetec <klhester3@gmail.com>
2020-10-02 03:09:19 -0500

    Enable tailwind css

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import "./App.css";
-
 function App() {
   return (
     <div className="App">
