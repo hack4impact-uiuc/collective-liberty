19fe50e5c3a42dcdb3b91f8c4151cdc236ea13c9
kenetec <klhester3@gmail.com>
2020-10-02 17:17:04 -0500

    🤡 Update tooling (#9)
    ### Changes
    * Enable tailwind css
    * Fix vercel.json (i.e. website link now works :D )
    * Update some ignore files
    * Add examples for folder structure

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,5 +1,15 @@
 //postcss.config.js
 const tailwindcss = require("tailwindcss");
 module.exports = {
-  plugins: [tailwindcss("./tailwind.js"), require("autoprefixer")],
+  plugins: [
+    tailwindcss("./tailwind.js"), 
+    require("autoprefixer"), 
+    process.env.NODE_ENV === 'production' && require('@fullhuman/postcss-purgecss')({
+      content: [
+        'src/**/*.{js,jsx}',
+        'public/index.html'
+      ],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    })
+  ],
 };
