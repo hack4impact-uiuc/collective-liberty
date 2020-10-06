38bd4ccba803ec980b8d279327ca733a48856221
kellydunleavy <39579211+kellydunleavy@users.noreply.github.com>
2020-10-05 21:29:17 -0500

    Adjustments to incident data model

--- api/src/models/incident.js
+++ api/src/models/incident.js
@@ -4,11 +4,12 @@ const mongoose = require("mongoose");
 const Incident = new mongoose.Schema({
     focus: { type: String, required: true },
     caseTag: { type: String, required: true },
-    dateOfOperation: { type: String, required: false },
-    operationType: { type: String: required: false },
+    dateOfOperation: { type: Date, required: false },
+    endDateOfOperation: { type: Date, required: false },
+    operationType: { type: String, required: false },
     city: { type: String, required: true },
     state: { type: String, required: false },
-    criminalCharges: { type: Boolean, required: false },
+    civilCodeCharges: { type: Boolean, required: false },
     notes: { type: String, required: false }
 });
 
