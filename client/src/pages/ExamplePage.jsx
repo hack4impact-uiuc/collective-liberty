726e5284100e2d77c59636ef3ae714bb9eb7c487
anoojlal <anoojl2@illinois.edu>
2020-10-16 17:56:43 -0400

    api function made for getting bounds of state

--- client/src/pages/ExamplePage.jsx
+++ client/src/pages/ExamplePage.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 import ExampleComponent from "../components/ExampleComponent";
+import { getBoundsOfState } from "../utils/nominatimApi";
+import { useEffect } from "react";
 
 const ExamplePage = () => {
+  useEffect(async () => {
+    // Testing response of getBoundsOfState api function
+
+    const response = await getBoundsOfState("Illinois");
+
+    if (response) {
+      console.log(response);
+    } else {
+      console.log("error");
+    }
+  });
+
   return (
     <>
       <ExampleComponent />
--- client/src/utils/nominatimApi.js
+++ client/src/utils/nominatimApi.js
@@ -4,12 +4,20 @@ const instance = axios.create({
   baseURL: "https://nominatim.openstreetmap.org",
 });
 
-export const getBoundsOfPlace = (place) => {
+export const getBoundsOfState = (place) => {
   const requestExtension = `search?q=${place}&format=jsonv2&polygon_geojson=1`;
+
   return instance.get(requestExtension).then(
-    (res) => res.data,
+    (res) => {
+      if (res.data[0].geojson.coordinates) {
+        return res.data[0].geojson.coordinates;
+      }
+
+      return null;
+    },
     (err) => {
       console.error(err);
+
       return null;
     }
   );
