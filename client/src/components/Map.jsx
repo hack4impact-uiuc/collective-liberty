0e470f59642f52c0787eb6bb622311e47a9bfb29
Albert Cao <albertc4@illinois.edu>
2020-10-19 22:03:16 -0500

    added basic components for zoom controls, search, and legend

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useState } from "react";
-import ReactMapGL from "react-map-gl";
+import ReactMapGL, { NavigationControl } from "react-map-gl";
+
+import "./../styles/Map.css";
 
 const LAT_BOUNDS = [25, 49];
 const LONG_BOUNDS = [-124, -68];
@@ -13,7 +15,7 @@ const Map = () => {
     height: "80vh",
     latitude: DEFAULT_COORDS[0],
     longitude: DEFAULT_COORDS[1],
-    zoom: DEFAULT_ZOOM
+    zoom: DEFAULT_ZOOM,
   });
 
   return (
@@ -48,7 +50,37 @@ const Map = () => {
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_API_KEY}
       dragRotate={false}
       touchRotate={false}
-    />
+    >
+      <div
+        className="searchBar"
+        style={{ position: "absolute", left: 20, top: 20 }}
+      >
+        <input
+          class="focus:outline-none focus:shadow-outline pl-2 mr-0.75 rounded-sm h-10"
+          type="search"
+          placeholder="Search for a location..."
+          aria-label="Search for a location"
+        ></input>
+        <button class="bg-white rounded-sm p-2 focus:outline-none h-10">
+          icon
+        </button>
+      </div>
+      <div
+        className="legend"
+        style={{ position: "absolute", right: 40, top: 20 }}
+      >
+        <details>
+          <summary>icon Legend</summary>
+          <div>Individual Arrests</div>
+        </details>
+      </div>
+      <div
+        className="navigationControl"
+        style={{ position: "absolute", right: 30, bottom: 50 }}
+      >
+        <NavigationControl />
+      </div>
+    </ReactMapGL>
   );
 };
 
