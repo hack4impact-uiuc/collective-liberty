c7f83bb36f240e3a3dc9cfea31fb4fb05631b9f5
kellydunleavy <39579211+kellydunleavy@users.noreply.github.com>
2020-10-19 21:21:08 -0500

    Seed Database (#24)
    The new script creates models from mock data and saves it to the db. Read XLSX file via Sheet.JS. Created a db object for each Incident (row in XLSX file), and saved this object to the database.
    
    Co-authored-by: kenetec <klhester3@gmail.com>

---
+++ api/src/utils/incident_parser.js
@@ -0,0 +1,38 @@
+/*
+ * this script is used to parse the provided
+ * mock data for incidents of human trafficking
+ */
+
+var XLSX = require('xlsx')
+var path = require('path');
+var mongoose = require('mongoose')
+var Incident = require('../models/Incident')
+require('dotenv').config()
+
+mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017', { useNewUrlParser: true, useUnifiedTopology: true })
+
+
+function main() {
+  const workbook = XLSX.readFile(path.join(__dirname, './incidents.xlsx'))
+  const worksheet = workbook.Sheets[workbook.SheetNames[0]]
+
+  const json = XLSX.utils.sheet_to_json(worksheet)
+  
+  for (const incident of json) {
+    let newIncident = new Incident({
+        focus: incident["Content/Focus"],
+        caseTag: incident["Case Tag"],
+        dateOfOperation: incident["Date of Operation"],
+        endDateOfOperation: incident["Date of Operation"],
+        operationType: incident["Operation Type"],
+        city: incident["Business City"],
+        state: incident["Business State"],
+        notes: incident["Notes"]
+        })
+
+    newIncident.save()
+    }
+
+};
+
+main()
