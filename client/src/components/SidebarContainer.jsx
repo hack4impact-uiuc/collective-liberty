a009007f0f60a3c573e8be5ffcf33dbd083865fd
Luciana Toledo-Lopez <lucianat@illinois.edu>
2020-10-21 13:44:27 -0500

    added placeholders in the SidebarContainer

--- client/src/components/SidebarContainer.jsx
+++ client/src/components/SidebarContainer.jsx
@@ -3,16 +3,13 @@ import React from "react";
 const SidebarContainer = () => {
   return (
     <div className="bg-black text-white p-6 shadow-md h-screen w-3/12">
-      <h2>Title</h2> 
+      <h2>Title</h2>
       <label for="period">year:</label>
-      <select className="period" id="period">
-      
-      </select>
-      <div className="graph">
-      </div>
+      <select className="period" id="period"></select>
+      <div className="graph"></div>
 
-      <button className="bg-orange-500 text-white font-sans py-2 px-4 rounded">
-          VIEW JOURNEYS
+      <button className="bg-orange-500 absolute inline-block text-white font-sans py-2 px-4 rounded">
+        VIEW JOURNEYS
       </button>
     </div>
   );
