6c0890b4a6751d02fb2ae11b3cfb6edfdb3beb50
Ayan Mallik <ayan.mallik20@gmail.com>
2020-10-21 21:45:37 -0500

    Fixed styling differences

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
@@ -9,38 +10,36 @@ const NavBar = () => {
           width="160"
           height="72"
           src="https://collectiveliberty.org/wp-content/uploads/2020/04/cropped-CollectiveLiberty_FullLogo_01_hi.png"
+          alt="Collective Liberty Logo"
         />
       </div>
-      <ul
-        role="navigation"
-        class="w-full block justify-end lg:flex lg:items-center lg:w-auto leading-none text-2xl lg:flex-grow"
-      >
+      <ul class="w-full block justify-end lg:flex lg:items-center lg:w-auto leading-none text-xl lg:flex-grow">
         <li>
-          <a
-            href=""
-            class="block mt-4 lg:inline-block lg:mt-0 p-8 text-white hover:text-blue-500 mr-10 bg-black"
+          <Link
+            to="/"
+            class="block lg:inline-block lg:mt-0 p-8 text-white bg-teal-900 border-b-2 border-teal-900 hover:text-blue-500"
             aria-label="Explore the Data"
           >
             Explore the Data
-          </a>
+          </Link>
         </li>
         <li>
-          <a
-            href=""
-            class="block mt-4 lg:inline-block lg:mt-0 p-8  text-black hover:text-blue-500 mr-10"
+          <Link
+            to="/"
+            class="block lg:inline-block lg:mt-0 p-8 text-teal-900 px-16 border-b-2 border-white hover:text-blue-500 hover:border-teal-900"
             aria-label="About Us"
           >
             About Us
-          </a>
+          </Link>
         </li>
         <li>
-          <a
-            href=""
-            class="block mt-4 lg:inline-block lg:mt-0 p-6 text-white hover:text-blue-500 bg-orange-500"
+          <Link
+            to="/"
+            class="block lg:inline-block lg:mt-0 p-6 text-white bg-orange-600 rounded hover:text-blue-500"
             aria-label="Donate Now"
           >
             Donate Now
-          </a>
+          </Link>
         </li>
       </ul>
     </nav>
