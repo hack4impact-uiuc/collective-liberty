4471716608f9f683e313ca7bd1a76545465de14b
Luciana Toledo-Lopez <lucianat@illinois.edu>
2020-10-21 20:51:39 -0500

    added divs and flexbox for elements

--- client/src/components/SidebarContainer.jsx
+++ client/src/components/SidebarContainer.jsx
@@ -2,15 +2,26 @@ import React from "react";
 
 const SidebarContainer = () => {
   return (
-    <div className="bg-black text-white p-6 shadow-md h-screen w-3/12">
-      <h2>Title</h2>
-      <label for="period">year:</label>
-      <select className="period" id="period"></select>
-      <div className="graph"></div>
+    <div className="flex flex-col bg-black p-6 shadow-md h-screen w-3/12">
+      <h2 className="Title">Name Of Location</h2>
+      <div clasName="flex flex-row text-grey">
+        <select
+          className="text-gray-700 text-center inline-block px-4 py-2 m-0"
+          id="start"
+        ></select>
+        <h1 className="text-gray-700 text-center inline-block py-2 m-2">to</h1>
+        <select
+          className="text-gray-700 text-center inline-block px-4 py-2 m-0"
+          id="end"
+        ></select>
+      </div>
+      <div className="graph m-20"></div>
 
-      <button className="bg-orange-500 absolute inline-block text-white font-sans py-2 px-4 rounded">
-        VIEW JOURNEYS
-      </button>
+      <div className="journeysButton absolute inset-x-0 bottom-0 m-20 ">
+        <button className="Journeys bg-orange-500 text-center text-white font-sans  py-2 px-4 rounded">
+          VIEW JOURNEYS
+        </button>
+      </div>
     </div>
   );
 };
