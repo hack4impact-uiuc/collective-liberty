f6f6b1855aa340de72782d78ac48fac76c6be6ed
kellydunleavy <39579211+kellydunleavy@users.noreply.github.com>
2020-10-22 23:38:28 -0500

    Work so far for GET endpoint for incidents with params

--- api/src/api/incidents.js
+++ api/src/api/incidents.js
@@ -8,4 +8,34 @@ router.get("/", async (req, res) => {
 	res.send(incidents)
 })
 
+router.get("/:focus", async (req, res) => {
+	const f = req.params.focus
+	const incidents = await Incident.findOne({focus: f})
+	res.send(incidents)
+})
+
+router.get("/:state", async (req, res) => {
+    let s = req.params.state;
+	const incidents = await Incident.findOne({state: s})
+	res.send(incidents)
+})
+
+router.get("/:city", async (req, res) => {
+	const c = req.params.city
+	const incidents = await Incident.findOne({city: c})
+	res.send(incidents)
+})
+
+router.get("/:start_date", async (req, res) => {
+	const s = req.params.dateOfOperation
+	const incidents = await Incident.findOne({dateOfOperation: s})
+	res.send(incidents)
+})
+
+router.get("/:end_date", async (req, res) => {
+	const e = req.params.endDateOfOperation
+	const incidents = await Incident.findOne({endDateOfOperation: e})
+	res.send(incidents)
+})
+
 module.exports = router;
