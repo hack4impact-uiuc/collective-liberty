2f910e105f58a306e618c9f88fdfcae8748479d4
kellydunleavy <39579211+kellydunleavy@users.noreply.github.com>
2020-10-23 00:20:50 -0500

    GET endpoint for incidents with parameters

--- api/src/api/incidents.js
+++ api/src/api/incidents.js
@@ -1,38 +1,31 @@
 const router = require('express').Router();
 const Incident = require('../models/Incident');
-var mongodb = require('mongodb');
 
-
-router.get("/", async (req, res) => {
-	const incidents = await Incident.find({})
-	res.send(incidents)
-})
-
-router.get("/:focus", async (req, res) => {
-	const f = req.params.focus
-	const incidents = await Incident.findOne({focus: f})
-	res.send(incidents)
-})
-
-router.get("/:state", async (req, res) => {
-    let s = req.params.state;
-	const incidents = await Incident.findOne({state: s})
-	res.send(incidents)
-})
-
-router.get("/:city", async (req, res) => {
+router.get("/city/:city", async (req, res) => {
 	const c = req.params.city
 	const incidents = await Incident.findOne({city: c})
 	res.send(incidents)
 })
 
-router.get("/:start_date", async (req, res) => {
+router.get("/focus/:focus", async (req, res) => {
+	const f = req.params.focus
+	const incidents = await Incident.findOne({focus: f})
+	res.send(incidents)
+})
+
+router.get("/state/:state", async (req, res) => {
+    let s = req.params.state;
+	const incidents = await Incident.findOne({state: s})
+	res.send(incidents)
+})
+
+router.get("/start_date/:start_date", async (req, res) => {
 	const s = req.params.dateOfOperation
 	const incidents = await Incident.findOne({dateOfOperation: s})
 	res.send(incidents)
 })
 
-router.get("/:end_date", async (req, res) => {
+router.get("/end_date/:end_date", async (req, res) => {
 	const e = req.params.endDateOfOperation
 	const incidents = await Incident.findOne({endDateOfOperation: e})
 	res.send(incidents)
