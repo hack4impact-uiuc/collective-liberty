f4dab11a57a635c02a3e3c9359dddb2f8d1600e9
Luciana Toledo-Lopez <lucianat@illinois.edu>
2020-10-23 18:07:36 -0500

    trying to resolve conflicts in ExamplePage.jsx

---
+++ client/src/pages/ExamplePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+
+const ExamplePage = () => {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+};
+export default ExamplePage;
