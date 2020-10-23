275e6b9b1334f78ee68a72bcf8f8cfbf97ed5a61
kenetec <klhester3@gmail.com>
2020-10-22 19:25:38 -0500

    Save

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,11 @@ import MapPage from "./pages/MapPage";
 function App() {
   return (
     <div className="App" role="main">
+      <link
+        href="http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext"
+        rel="stylesheet"
+        type="text/css"
+      ></link>
       <Router>
         <div>
           <Switch>
