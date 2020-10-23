31433653034d184d62c9ebb3c1817f718f4ece0d
kellydunleavy <39579211+kellydunleavy@users.noreply.github.com>
2020-10-23 16:58:33 -0500

    Fixes based on Feedback

--- api/src/api/incidents.js
+++ api/src/api/incidents.js
@@ -7,7 +7,7 @@ router.get('*', async (req, res) => {
 	const state = req.query.state;
 	const start_date = req.query.dateOfOperation
 	const end_date = req.query.endDateOfOperation
-	const incidents = await Incident.find({$or: [{city: city}, {focus: focus}, {state: state}, {dateOfOperation: start_date}, {endDateOfOperation: end_date}]})
+	const incidents = await Incident.find(req.query)
 	res.send(incidents)
 })
 
