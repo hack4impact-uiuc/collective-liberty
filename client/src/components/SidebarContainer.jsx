a1b1cd27298d0fbcc3df7d618fff2d21a9e5ac3b
Siraj Chokshi <19193347+SirajChokshi@users.noreply.github.com>
2020-10-29 15:31:00 -0400

    📈 Sidebar line chart (#38)
    * 📚 add policy timeline
    
    * 📈 add chart styles
    
    * 🛠️ fix timeline date clipping

--- client/src/components/SidebarContainer.jsx
+++ client/src/components/SidebarContainer.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Line } from "react-chartjs-2";
+import SidebarChart from "./SidebarChart";
 
 const SidebarContainer = () => {
   return (
     <div className="flex flex-col bg-black p-6 shadow-md h-screen w-3/12 container">
-      <h2 className="Title text-white">Name Of Location</h2>
+      <h2 className="Title text-white">Name of Location</h2>
       <div className="flex flex-row text-grey">
         <select
           aria-label="beginning year of time range"
@@ -22,12 +22,10 @@ const SidebarContainer = () => {
           <option aria-label="2020">2020</option>
         </select>
       </div>
-      <div className="graph m-20"></div>
-      <div className="graph m-20">
-        <Line />
-      </div>
 
-      <div className="journeysButton flex justify-center bottom-0 pt-40 m-20">
+      <SidebarChart arrests={null} laws={null} />
+
+      <div className="journeysButton flex justify-center m-20">
         <button
           aria-label="view journeys"
           className="Journeys bg-orange-500 text-center text-white font-sans py-2 px-4 rounded"
