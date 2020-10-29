d9e59641bdeafa9a224af8164e868afab66583c7
Siraj <sirajchokshi@gmail.com>
2020-10-29 04:41:06 -0500

    📌 add arrests endpoint

---
+++ api/src/api/arrests.js
@@ -0,0 +1,56 @@
+const router = require('express').Router();
+const Incident = require('../models/Incident');
+
+const VICTIM_ARREST_FOCUSES = [
+	"Prostitution Arrests or Stings"
+]
+
+function getStatsFromArrests(arrests) {
+
+	let victimArrestCount = 0;
+
+	if (arrests.length > 0) {
+		victimArrestCount = arrests
+		.map(arrest => VICTIM_ARREST_FOCUSES.includes(arrest.focus) ? 1 : 0)
+		.reduce((accumulator, arrest) => accumulator + arrest)
+	}
+
+	return {
+		arrestScore: Number(((arrests.length - victimArrestCount) / arrests.length * 100).toFixed(2)),
+		victimArrestCount: victimArrestCount,
+		traffickerArrestCount: arrests.length - victimArrestCount,
+		totalCaseCount: arrests.length
+	}
+
+}
+
+router.get('*', async (req, res) => {
+
+	const query = {};
+
+	if (req.query.city) {
+		query.city = req.query.city;
+	}
+	if (req.query.state) {
+		query.state = req.query.state;
+	}
+	if (req.query.time_range) {
+
+		const [startDate, endDate] = req.query.time_range.split(',');
+
+		if (!isNaN(Number(startDate)) && !isNaN(Number(endDate))) {
+			query.dateOfOperation = {
+				$gte: startDate
+			};
+			query.endDateOfOperation = {
+				$lte: endDate
+			};
+		}
+	}
+
+	const arrests = await Incident.find(query)
+	const stats = getStatsFromArrests(arrests)
+	res.send(stats)
+})
+
+module.exports = router;
--- api/src/api/index.js
+++ api/src/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const incidents = require('./incidents');
+const arrests = require('./arrests');
 
 router.use('/incidents', incidents);
+router.use('/arrests', arrests);
 
 module.exports = router;
