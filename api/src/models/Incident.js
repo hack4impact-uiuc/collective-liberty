d2f67ca737ccf06dfba5b06ecbbf63be6c055bf0
kenetec <klhester3@gmail.com>
2020-10-29 16:50:48 -0500

    Update Incident model and normalize date parsing

--- api/src/models/Incident.js
+++ api/src/models/Incident.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 const Incident = new mongoose.Schema({
   focus: { type: String, required: true },
   caseTag: { type: String, required: true },
-  dateOfOperation: { type: String, required: false },
-  endDateOfOperation: { type: String, required: false },
+  dateOfOperation: { type: Number, required: false },
+  endDateOfOperation: { type: Number, required: false },
   operationType: { type: String, required: false },
   city: { type: String, required: true },
   state: { type: String, required: false },
