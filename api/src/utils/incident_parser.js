4c246af060c7262e31c1baa71b2b1a4f0e338604
kenetec <klhester3@gmail.com>
2020-10-30 12:31:21 -0500

    🍯 Improve excel parsing (#41)
    * Remove getting-started folder and fix font link
    
    * Add random incident generator
    
    * Update Incident model and normalize date parsing
    
    * Add random incident generator

--- api/src/utils/incident_parser.js
+++ api/src/utils/incident_parser.js
@@ -7,6 +7,7 @@ const XLSX = require('xlsx');
 const path = require('path');
 const mongoose = require('mongoose');
 const Incident = require('../models/Incident');
+const dateParser = require('./date_parser');
 require('dotenv').config();
 
 mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017', {
@@ -21,11 +22,24 @@ function main() {
   const json = XLSX.utils.sheet_to_json(worksheet);
 
   for (const incident of json) {
+    const dateOfOperationStrs = dateParser.parse(incident['Date of Operation']);
+    let dateOfOperation = new Date('1/1/2000').getTime();
+    let endDateOfOperation = dateOfOperation;
+
+    if (dateOfOperationStrs.length > 0) {
+      dateOfOperation = new Date(dateOfOperationStrs[0]).getTime();
+      endDateOfOperation = dateOfOperation;
+
+      if (dateOfOperationStrs.length > 1) {
+        endDateOfOperation = new Date(dateOfOperationStrs[1]).getTime();
+      }
+    }
+
     let newIncident = new Incident({
       focus: incident['Content/Focus'],
       caseTag: incident['Case Tag'],
-      dateOfOperation: incident['Date of Operation'],
-      endDateOfOperation: incident['Date of Operation'],
+      dateOfOperation,
+      endDateOfOperation,
       operationType: incident['Operation Type'],
       city: incident['Business City'],
       state: incident['Business State'],
