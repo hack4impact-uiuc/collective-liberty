1e41c0a51d6e6840da385c604d828e6162a74e35
Siraj Chokshi <19193347+SirajChokshi@users.noreply.github.com>
2020-10-30 17:28:10 -0400

    🧭 Navbar Updates (#42)
    * 📎 add route reactivity to navbar
    
    * 🎨 update navbar styling

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,5 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
+// styles
+const linkClasses =
+  "block lg:inline-block lg:mt-0 p-8 text-md font-semibold txt-dark-green px-16 border-b-4 border-white hover-border-dark-green";
+const activeLinkClasses =
+  "text-white bg-dark-green border-b-4 border-dark-green";
+const navButtonClasses =
+  "block lg:inline-block lg:mt-0 p-6 pr-12 pl-12 mr-4 ml-4 text-white text-md font-semibold bg-orange rounded";
 
 const NavBar = () => {
   return (
@@ -15,31 +23,31 @@ const NavBar = () => {
       </div>
       <ul class="w-full block justify-end lg:flex lg:items-center lg:w-auto leading-none text-md lg:flex-grow">
         <li>
-          <Link
+          <NavLink
             to="/"
-            class="block lg:inline-block lg:mt-0 p-8 text-white bg-dark-green border-b-2 border-dark-green hover:text-blue-500"
+            className={linkClasses}
+            activeClassName={activeLinkClasses}
             aria-label="Explore the Data"
+            exact
           >
             Explore the Data
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link
-            to="/"
-            class="block lg:inline-block lg:mt-0 p-8 txt-dark-green px-16 border-b-2 border-white hover:text-blue-500 hover:border-teal-900"
+          <NavLink
+            to="/about"
+            className={linkClasses}
+            activeClassName={activeLinkClasses}
             aria-label="About Us"
+            exact
           >
             About Us
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link
-            to="/"
-            class="block lg:inline-block lg:mt-0 p-6 mr-4 text-white bg-orange rounded hover:text-blue-500"
-            aria-label="Donate Now"
-          >
+          <a href="#donate" class={navButtonClasses} aria-label="Donate Now">
             Donate Now
-          </Link>
+          </a>
         </li>
       </ul>
     </nav>
