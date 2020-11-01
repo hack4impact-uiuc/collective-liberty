d9409ec7166483d16cec050f726c2f5a6027c84a
kellydunleavy <39579211+kellydunleavy@users.noreply.github.com>
2020-11-01 00:59:37 -0500

    Cleaned unused code

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -4,17 +4,6 @@ const instance = axios.create({
   baseURL: "http://localhost:5000/api",
 });
 
-/*export const getIncidents = (focus,state,start_date,end_date) => {
-    const requestString = `/incidents?focus=${focus}&state=${state}&start_date=${start_date}&end_date=${end_date}`;
-    return instance.get(requestString).then(
-        res => res.data,
-        err => {
-            console.error(err);
-            return null;
-        },
-    );
-};
-*/
 export const getIncidentsByState = (state) => {
   const requestString = `/incidents?state=${state}`;
   return instance.get(requestString).then(
