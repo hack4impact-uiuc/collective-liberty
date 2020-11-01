467fcc4c8316befded96318cff4451dcd94a425e
Albert Cao <albertc4@illinois.edu>
2020-10-31 21:06:56 -0500

    added frontend fetch for incidents data

--- client/src/pages/MapPage.jsx
+++ client/src/pages/MapPage.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Map from "../components/Map";
 import NavBar from "../components/NavBar";
 import TimeRange from "../components/TimeRange";
 import SidebarContainer from "../components/SidebarContainer";
+
+import { getIncidents } from "../utils/api";
 
 import "./../styles/MapPage.css";
 
@@ -14,6 +16,16 @@ const step = 1;
 
 const MapPage = () => {
   const [range, setRange] = useState(initRange);
+  const [incidents, setIncidents] = useState([]);
+
+  const fetchIncidents = async (params) => {
+    const res = await getIncidents(params);
+    setIncidents(res);
+  }
+
+  useEffect(() => {
+    fetchIncidents({});
+  }, []);
 
   return (
     <>
