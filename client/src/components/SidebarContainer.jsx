90671039afba5e5d61c125e44e3174714835cd7e
lucianatl <55062455+lucianatl@users.noreply.github.com>
2020-11-02 20:01:18 -0600

    🍩 Trafficking scores (#48)
    * added sidebar conatiner div
    
    * trying to delete yarn.lock file, extra changes in SidebarContainer
    
    * fixed changes in yarn.lock
    
    * added placeholders in the SidebarContainer
    
    * added divs and flexbox for elements
    
    * changed font color
    
    * added react-chartjs-2 dependency for line graph
    
    * added yarn add react-chartjs-2 as dependency
    
    * ran yarn add chart.js in client
    
    * added aria labels to slectors and button, made the contianer div responsive
    
    * deleted example page
    
    * trying to resolve conflicts in ExamplePage.jsx
    
    * commiting changes in sidebar
    
    * commiting changes in sidebar and api
    
    * resolving conflict in api.js
    
    * finish overlay for score, minor style changes
    
    Co-authored-by: Luciana Toledo-Lopez <toledolu2019@u4sd.org>
    Co-authored-by: Siraj <sirajchokshi@gmail.com>

--- client/src/components/SidebarContainer.jsx
+++ client/src/components/SidebarContainer.jsx
@@ -1,5 +1,10 @@
+//@flow
 import React, { useEffect, useState } from "react";
 import SidebarChart from "./SidebarChart";
+import { Doughnut } from "react-chartjs-2";
+import { getArrestData } from "../utils/traffickingstatsapi";
+
+import "../styles/SidebarContainer.css";
 
 const selectClasses =
   "block appearance-none bg-black txt-gray font-semibold text-lg pl-0 py-2 pr-6 rounded leading-tight focus:outline-none";
@@ -17,6 +22,8 @@ const DropdownArrow = () => (
 );
 
 type Props = {
+  city: String,
+  state: String,
   range: [Int],
   setRange: ([Int]) => void,
   minTime: Int,
@@ -36,6 +43,33 @@ const SidebarContainer = (props: Props) => {
     }
     setYears(newYears);
   }, [maxTime, minTime, setYears, step]);
+  const { city, state, time_range } = props;
+  const [arrestData, setArrestData] = useState(null);
+  useEffect(async () => {
+    await getArrestData({
+      city: "",
+      state: "Illinois",
+      range: [2000, 2020],
+    }).then((data) => {
+      setArrestData(data);
+    });
+  }, []);
+
+  const donutData = {
+    datasets: [
+      {
+        label: "State score",
+        data: [
+          100 - (arrestData ? arrestData.arrestScore : 0),
+          arrestData ? arrestData.arrestScore : 0,
+        ],
+        backgroundColor: ["rgba(255, 255, 255, 1)", "rgba(60, 179, 113, 1)"],
+        borderColor: ["rgba(255, 255, 255, 1)", "rgba(60, 179, 113, 1)"],
+        borderWidth: 1,
+        weight: 4,
+      },
+    ],
+  };
 
   return (
     <div
@@ -83,6 +117,54 @@ const SidebarContainer = (props: Props) => {
         </div>
       </div>
 
+      <div className="TraffickingStats flex flex-row m-1 pt-3 pb-1 ">
+        <div className="TraffickingScore w-full relative" style={{ flex: 1 }}>
+          <div className="score">
+            <Doughnut
+              data={donutData}
+              options={{
+                maintainAspectRatio: true,
+                cutoutPercentage: 72,
+              }}
+            />
+          </div>
+          <div
+            className="score overlay absolute text-white font-semibold p-4 text-2xl"
+            style={{
+              top: "7.5px",
+              left: "calc(50% - 1.667vw)",
+              textAlign: "center",
+            }}
+          >
+            {arrestData && arrestData.arrestScore.toFixed(0)}
+          </div>
+        </div>
+        <div
+          className="ArrestTypes flex flex-col pt-5 ml-5 -mt-1"
+          style={{ flex: 3 }}
+        >
+          <h2 className="TraffickerArrests txt-gray text-sm">
+            {" "}
+            {arrestData && arrestData.traffickerArrestCount} Trafficker Arrests
+          </h2>
+          <hr size="5" className="my-1" width="90%" color="#cccccc"></hr>
+          <h2 className="VictimArrests txt-gray text-sm">
+            {arrestData && arrestData.victimArrestCount} Victim Arrests
+          </h2>
+        </div>
+        <div className="Separation flex flex-row px-1 mt-1"></div>
+        <div
+          className="TotalCases flex flex-col"
+          style={{ flex: 2, alignItems: "center" }}
+        >
+          <div className="totalCaseCount text-white px-5 text-3xl mt-2">
+            {arrestData && arrestData.totalCaseCount}
+          </div>
+          <h2 className="totalCasesLabel txt-gray text-sm -mt-1">
+            Total Cases
+          </h2>
+        </div>
+      </div>
       <SidebarChart arrests={null} laws={null} />
       {/* 
       <div className="journeysButton flex justify-center mt-10">
