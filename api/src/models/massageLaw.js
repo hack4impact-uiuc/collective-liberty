41104a130e466c56eeacaf36c82088712f9bb806
kellydunleavy <39579211+kellydunleavy@users.noreply.github.com>
2020-11-09 22:17:38 -0600

    Fixing MassageLawSchema

--- api/src/models/massageLaw.js
+++ api/src/models/massageLaw.js
@@ -4,9 +4,7 @@ const mongoose = require('mongoose');
 const MassageLaw = new mongoose.Schema({
   state: { type: String, required: false },
   city: { type: String, required: false },
-  strengthOfStateLaw: { type: String, required: false },
-  codeLink: { type: String, required: false },
-  codeSection: { type: String, required: false },
+  strengthOfLaw: { type: String, required: false },
 });
 
 module.exports = mongoose.model('MassageLaw', MassageLaw);
