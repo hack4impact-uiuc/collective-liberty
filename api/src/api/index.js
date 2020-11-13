d2c903ca9076c120db29ed2f7cb1dd0d6715265f
lucianatl <55062455+lucianatl@users.noreply.github.com>
2020-11-12 20:54:22 -0600

    🇺🇸 All Incidents enpoint (#63)
    * allIncidents is working
    
    * returns correct data within the time range
    
    * added changes requested by kenetec
    
    * adding client/src/state_boundaries.json

--- api/src/api/index.js
+++ api/src/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const incidents = require('./incidents');
 const arrests = require('./arrests');
+const allIncidents = require('./allIncidents');
 
 router.use('/incidents', incidents);
 router.use('/arrests', arrests);
+router.use('/allincidents', allIncidents);
 
 module.exports = router;
--- api/src/api/policies.js
+++ api/src/api/policies.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const Law = require('../models/Law');
 
 router.get('*', async (req, res) => {
-    const laws = await Law.find(req.query);
-    res.send(laws);
+  const laws = await Law.find(req.query);
+  res.send(laws);
 });
 
 module.exports = router;
