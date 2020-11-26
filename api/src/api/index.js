1aa3732894b9f43f45174ba463849a56fca089e7
Albert Cao <albertc4@illinois.edu>
2020-11-26 13:58:43 -0600

    created get endpoints

--- api/src/api/index.js
+++ api/src/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const incidents = require('./incidents');
 const arrests = require('./arrests');
 const allIncidents = require('./allIncidents');
+const policies = require('./policies');
 
 router.use('/incidents', incidents);
 router.use('/arrests', arrests);
 router.use('/allincidents', allIncidents);
+router.use('/policies', policies);
 
 module.exports = router;
