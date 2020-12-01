333fd8022a74fd3bf1df4912ce65bda147bf9969
kenetec <klhester3@gmail.com>
2020-12-01 00:34:28 -0600

    Remove temp file saving

--- api/src/api/csvUpload.js
+++ api/src/api/csvUpload.js
@@ -1,21 +1,19 @@
 const router = require('express').Router();
 const multer = require('multer');
 const csv = require('fast-csv');
-const upload = multer({ dest: './src/tmp/csv/' });
-const fs = require('fs');
+const upload = multer();
 
 router.post('/', upload.single('file'), function (req, res) {
   const fileRows = [];
 
   // open uploaded file
   csv
-    .parseFile(req.file.path)
+    .parseString(req.file.buffer.toString())
     .on('data', function (data) {
       fileRows.push(data); // push each row
     })
     .on('end', function () {
       console.log(fileRows);
-      fs.unlinkSync(req.file.path); // remove temp file
       //process "fileRows" and respond
       res.json({});
       res.status(200);
