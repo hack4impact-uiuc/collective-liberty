956697e40e8770d795764279d294cc56dbdea129
kenetec <klhester3@gmail.com>
2020-12-03 04:19:18 -0600

    Add /login/redirect

--- api/src/api/login.js
+++ api/src/api/login.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const passport = require('passport');
 
+// shoutout to Josh Byster for being a god
+
 router.get('/', (req, res, next) => {
   const baseUrl = `${req.protocol}://${req.get('host')}`;
   const state = baseUrl
@@ -14,22 +16,31 @@ router.get('/', (req, res, next) => {
   auth(req, res, next);
 });
 
-router.get(
-  '/callback',
-  (req, res, next) => {
+router.get('/redirect', (req, res) => {
+  try {
     const { state } = req.query;
     const { baseUrl } = JSON.parse(Buffer.from(state, 'base64').toString());
-    const verify = passport.authenticate('google', {
-      failureRedirect: `${baseUrl}/login`,
-    });
 
     if (typeof baseUrl === 'string') {
-      verify();
+      return res.redirect(
+        `${baseUrl}/api/login/callback?${req._parsedUrl.query}`
+      );
     }
 
-    verify(req, res, next);
-  },
-  (req, res) => res.redirect(`${req.protocol}://${req.get('host')}/uploadData`)
+    return res.redirect('/api/login/callback');
+  } catch (e) {
+    return res.status(403).json({
+      code: 403,
+      message: 'Failed to redirect',
+      success: false,
+    });
+  }
+});
+
+router.get(
+  '/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  (req, res) => res.redirect('/uploadData')
 );
 
 module.exports = router;
