ed375f795247354d4d12f3f1554461e58d68fb04
anoojlal <anoojl2@illinois.edu>
2020-12-02 20:38:33 -0600

    Clicking Login with Google redirects to /api/login

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -21,7 +21,7 @@ const LoginPage = () => {
         </a>
         <br />
         <br />
-        <a className="loginWithGoogle">
+        <a className="loginWithGoogle" href={"/api/login"}>
           <img
             width="25"
             src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png"
