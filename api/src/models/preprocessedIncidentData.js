8b103885ebb9f83eaa44970b329cc737d5771783
kellydunleavy <39579211+kellydunleavy@users.noreply.github.com>
2020-12-22 20:32:46 -0600

    Switched from type Dictionary to Map because Dictionary is not a permitted SchemaType for mongoose

--- api/src/models/preprocessedIncidentData.js
+++ api/src/models/preprocessedIncidentData.js
@@ -3,14 +3,10 @@ const mongoose = require('mongoose');
 
 const PreprocessedIncidentData = new mongoose.Schema({
   fileName: { type: String, required: true },
-  yearCounts: { type: Dictionary, schema: { year: Number,
-      count: Number }, required: false },  
-  incidentTypeCounts: { type: Dictionary, schema: { incidentType: String,
-      count: Number }, required: false  },
-  cityCounts: { type: Dictionary, schema: { city: String,
-      count: Number }, required: false },
-  stateCounts: { type: Dictionary, schema: { state: String,
-      count: Number }, required: false  },
+  yearCounts: { type: Map, of: String, required: false },  
+  incidentTypeCounts: { type: Map, of: String, required: false  },
+  cityCounts: { type: Map, of: String, required: false },
+  stateCounts: { type: Map, of: String, required: false  },
 });
 
 module.exports = mongoose.model('PreprocessedIncidentData', PreprocessedIncidentData);
