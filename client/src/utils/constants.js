28e298e721931f2f3032564fc5b56fead0f01f2d
kenetec <klhester3@gmail.com>
2021-01-02 09:42:40 -0600

    Remove large comment

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -76,16 +76,6 @@ export const ARRESTS_CHART_TITLE = "How Law Correlates with Arrests";
 export const MASSAGE_LAWS_CHART_TITLE =
   "How Law Correlates with Massage Parlor Incidents";
 
-// export const massageParlorStrengthExplanations = Object.freeze({
-//   None: 'There are no laws governing massage establishments. In the case of counties of cities they can sometimes draw on state laws but experience has proven that local inspectors and civil enforcement equipped with local laws can bring consequences to traffickers that just relying on state laws cannot.',
-//   "Very Bad": 'These laws frequently categorize all massage businesses as sexually-oriented. Not only does this make it exponentially harder to help potential trafficking victims, it puts licensed practitioners at higher risk of sexual harassment and assault because their therapeutic practices are labeled the same as strip clubs and pornographic video stores.',
-//   Bad: 'The law targets the behavior of workers and targets the consequences to the workers as well, placing all of the burden on potential trafficking victims rather than the managers and operatives controlling their behavior.',
-//   Weak: 'The law has minimal clauses to regulate businesses - usually just zoning restrictions or requirements to obtain a business license.',
-//   Fair: 'The jurisdiction has passed a law that attempts to regulate businesses to close exploitative and/or trafficking sites but lacks sufficient clauses to reliably close such venues.',
-//   Good: 'This law contains at least 3 of the clauses that have proven successful at closing trafficking venues while placing minimum burdens on licensed practitioners.',
-//   Strong: 'This law contains at least 3 of the clauses that have proven successful at closing trafficking venues while placing minimum burdens on licensed practitioners.',
-// })
-
 export const VACATUR_RANK_EXPLANATIONS = Object.freeze({
   Excellent:
     "These states provide robust avenues for civil relief that are trauma-informed and allow survivors of trafficking to live free of documentation that records them as criminals rather than survivors.",
