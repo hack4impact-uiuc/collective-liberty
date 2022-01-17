e82a5c9a0ffae0ffb08edb7fec54bd3e3b7fc91f
kenetec <klhester3@gmail.com>
2022-01-17 02:04:51 -0600

    Re-add navbar common changes

--- client/src/components/NavBarCommon.jsx
+++ client/src/components/NavBarCommon.jsx
@@ -3,15 +3,15 @@ import { NavLink } from "react-router-dom";
 import { USER_ROLES } from "../utils/constants";
 import { useAuth } from "../utils/useAuth";
 import { getUserRole } from "../utils/api";
-import useWindowDimensions from '../utils/mobile';
-import NavBar from './desktop/NavBar';
-import NavBarMobile from './mobile/NavbarMobile';
+import useWindowDimensions from "../utils/mobile";
+import NavBar from "./desktop/NavBar";
+import NavBarMobile from "./mobile/NavbarMobile";
 
 const NavBarCommon = (props) => {
-  const [windowDimensions, isMobile] = useWindowDimensions();
+  const [, isMobile] = useWindowDimensions();
 
-  if (isMobile) return <NavBarMobile {...props}/>;
-  return <NavBar {...props}/>
+  if (isMobile) return <NavBarMobile {...props} />;
+  return <NavBar {...props} />;
 };
 
 export default NavBarCommon;
